1ad7c83e476f02e8826728e531f9796b413455fd
Vilmantas Runas <118439250+vilmantas-r@users.noreply.github.com>
2023-01-12 14:16:52 +0200

    Added additional test scenarios to Feign Demo

--- feign-demo/src/test/java/com/nortal/test/feign/demo/api/CatApi.java
+++ feign-demo/src/test/java/com/nortal/test/feign/demo/api/CatApi.java
@@ -5,15 +5,46 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @FeignClient(name = "catApi", url = "https://catfact.ninja")
 public interface CatApi {
 
     @GetMapping("/fact")
     ResponseEntity<Fact> getFact();
 
+    @GetMapping("/facts")
+    ResponseEntity<FactsList> getFacts();
+
+    @GetMapping("/breeds")
+    ResponseEntity<BreedsList> getBreeds();
+
     @Data
     class Fact {
         private String fact;
         private Integer length;
+    }
+
+    @Data
+    class FactsList {
+        private List<Fact> data;
+        private Integer current_page;
+        private Integer last_page;
+    }
+
+    @Data
+    class Breed {
+        private String breed;
+        private String country;
+        private String origin;
+        private String coat;
+        private String pattern;
+    }
+
+    @Data
+    class BreedsList {
+        private List<Breed> data;
+        private Integer current_page;
+        private Integer last_page;
     }
 }
